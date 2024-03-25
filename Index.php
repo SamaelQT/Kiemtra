495f2a34e3c763e0d8a5ea45c7a647a64d37b0f2
Your Name <you@example.com>
2024-03-25 13:38:13 +0700

    Initial commit

---
+++ Index.php
@@ -0,0 +1,11 @@
+<?php include("header.php"); ?>
+<ul class="menu">
+    <li>
+        <a href="/lab3/list_product.php">Danh sach san pham </a>
+
+    </li>
+    <li>
+        <a href="/lab3/add_product.php">Them san pham</a>
+    </li>
+</ul>
+<?php include_once("footer.php");
---
+++ footer.php
@@ -0,0 +1,6 @@
+</div>
+<footer>
+    &#169; 2024 - Hồ Hữu Quang - Faculty of Information Technology (HUTECH)
+</footer>
+</body>
+</html>
